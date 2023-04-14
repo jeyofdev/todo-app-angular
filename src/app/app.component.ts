fc52576ecbdcb809698ed3ca2da8545362f2f26b
jeyofdev <jgregoire.pro@gmail.com>
2023-04-14 21:54:10 +0200

    feat : todos filters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { TodoModel } from './core/models/todo.model';
 import { Component, OnInit } from '@angular/core';
 import { DarkModeEnum } from './core/types/enums';
 import { TodoService } from './core/services/todo.service';
-import { Observable } from 'rxjs';
+import { Observable, map } from 'rxjs';
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faCheck, faList, faXmark } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
 	selector: 'app-root',
@@ -13,6 +15,10 @@ export class AppComponent implements OnInit {
 	title!: string;
 	darkMode!: DarkModeEnum;
 	todosList$!: Observable<TodoModel[]>;
+	todosListFiltered$!: Observable<TodoModel[]>;
+	iconAll!: IconDefinition;
+	iconNotCompleted!: IconDefinition;
+	iconCompleted!: IconDefinition;
 
 	constructor(private todoService: TodoService) {}
 
@@ -20,7 +26,11 @@ export class AppComponent implements OnInit {
 		this.title = 'Todo app';
 		this.darkMode = DarkModeEnum.LIGHT;
 		this.todosList$ = this.todoService.getAllTodos();
+		this.todosListFiltered$ = this.todosList$;
 		this.todoService.getAllTodos();
+		this.iconAll = faList;
+		this.iconNotCompleted = faXmark;
+		this.iconCompleted = faCheck;
 	}
 
 	OnSwitchDarkMode(): void {
@@ -28,6 +38,17 @@ export class AppComponent implements OnInit {
 			this.darkMode = DarkModeEnum.DARK;
 		} else {
 			this.darkMode = DarkModeEnum.LIGHT;
+		}
+	}
+
+	onFilteredList(status: string | null): void {
+		if (status) {
+			// eslint-disable-next-line no-console
+			this.todosListFiltered$ = this.todosList$.pipe(
+				map(todos => todos.filter(t => t.status === status)),
+			);
+		} else {
+			this.todosListFiltered$ = this.todosList$;
 		}
 	}
 }
