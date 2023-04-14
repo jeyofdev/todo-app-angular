e70228f948c02b8335547d6df3154763f1d86daf
jeyofdev <jgregoire.pro@gmail.com>
2023-04-14 15:08:06 +0200

    feat : input to add new todo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { TypographyComponent } from './core/components/ui/typography/typography.component';
 import { DarkModeButtonComponent } from './core/components/ui/dark-mode-button/dark-mode-button.component';
+import { TodoFormComponent } from './core/components/ui/form/todo-form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-	declarations: [AppComponent, TypographyComponent, DarkModeButtonComponent],
-	imports: [BrowserModule, FontAwesomeModule],
+	// eslint-disable-next-line prettier/prettier
+	declarations: [
+		AppComponent,
+		TypographyComponent,
+		DarkModeButtonComponent,
+		TodoFormComponent,
+	],
+	imports: [BrowserModule, FontAwesomeModule, FormsModule],
 	providers: [],
 	bootstrap: [AppComponent],
 })
